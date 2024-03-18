d5f0d9629bcf7c1bdff80a63e8f24771dfd6a4cd
IamYud <vuanduy2004@gmail.com>
2024-03-18 20:45:32 +0700

    bai9

--- LAB2/src/Bai9.java
+++ LAB2/src/Bai9.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 public class Bai9 {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("nhap chuoi ky tu : ");
